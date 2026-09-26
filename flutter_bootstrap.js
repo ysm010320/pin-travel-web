ce608f3e16a65a50f9822c602e5cbd1e9e759d06
ysm010320 <ysm010320@users.noreply.github.com>
2026-09-26 11:21:08 +0900

    Redeploy with OpenFreeMap Positron basemap.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1012448548" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1693883727" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
